dd68c525d8a6ee810dd7e0e5c6a677149eb9aa85
AnTruongQuoc <truongquocan123456@gmail.com>
2020-12-03 11:17:40 +0700

    Recontruct and add app.js

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const router = express.Router();
-const fs=require('fs');
+
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+
+var hbs = require('express-handlebars')
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + '/views/prtials'
+}));
+app.set('view engine', 'hbs')
 
 
+
+app.get('/',(req,res)=>{
+    res.locals.title = "Jeopardize Contest"
+	res.render('index')
+})
+
+app.get('/task1',(req,res)=>{
+    res.locals.title = "Jars Saving"
+	res.render('task1')
+})
+
+app.get('/task2',(req,res)=>{
+    res.locals.title = "Inspiring Quotes"
+	res.render('task2')
+})
+
+app.get('/task3',(req,res)=>{
+    res.locals.title = "TV Sales"
+	res.render('task3')
+})
+
+app.get('/task4',(req,res)=>{
+    res.locals.title = "Zodiac Characteristics"
+	res.render('task4')
+})
+
+app.get('/task4-details',(req,res)=>{
+    res.locals.title = "Zodiac Characteristics"
+	res.render('task4-details')
+})
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
