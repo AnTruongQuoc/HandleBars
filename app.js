89029e2cad8609113a6366d2c57b7726c2751817
nateclint99 <giahungnguyen999@gmail.com>
2020-12-03 11:34:04 +0700

    Updated Task4

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const data = require("./public/data")
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
@@ -10,7 +12,7 @@ app.engine('hbs', hbs({
     extname: 'hbs',
     defaultLayout: 'layout',
     layoutsDir: __dirname + "/views/layouts",
-    partialsDir: __dirname + '/views/prtials'
+    partialsDir: __dirname + '/views/partials'
 }));
 app.set('view engine', 'hbs')
 
@@ -43,6 +45,14 @@ app.get('/task4',(req,res)=>{
 
 app.get('/task4-details',(req,res)=>{
     res.locals.title = "Zodiac Characteristics"
+    res.locals.name = data.zodiacs[4]['name']
+    res.locals.imagePath = '/images' + data.zodiacs[4]['imagePath']
+    res.locals.dates = data.zodiacs[4]['dates']
+    res.locals.strengths = data.zodiacs[4]['strengths']
+    res.locals.weaknesses = data.zodiacs[4]['weaknesses']
+    res.locals.likes = data.zodiacs[4]['likes']
+    res.locals.dislikes = data.zodiacs[4]['dislikes']
+    res.locals.description = data.zodiacs[4]['description']
 	res.render('task4-details')
 })
 
